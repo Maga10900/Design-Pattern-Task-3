03cdd93f460c6240d9be6a53e88b5b482cc09bd8
Maga10900 <maga168123@gmail.com>
2024-06-05 07:51:59 +0400

    DP Task 3

---
+++ AdapterdesignPattern/IDevice.cs
@@ -0,0 +1,31 @@
+﻿namespace AdapterdesignPattern;
+
+public interface IDevice
+{
+    void Work();
+}
+
+public class Mouse : IDevice
+{
+    public void Work()
+    {
+        //do something
+    }
+}
+//Nugetden gelib
+public class MouseNuget
+{
+    public void DoWork()
+    {
+        //do Something
+    }
+}
+
+public class MouseNugetAdapter : IDevice
+{
+    public void Work()
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ AdapterdesignPattern/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdapterdesignPattern;
+
+Console.WriteLine("Hello, World!");
+
+List<IDevice> devices = new List<IDevice>();
+foreach (var device in devices)
+{
+    device.Work();
+}
